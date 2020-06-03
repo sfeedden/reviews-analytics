a5e6b72a4f1a7685410783f811f2002cc115a389
Steven Liang <gkg.yji@gmail.com>
2020-06-04 00:39:03 +0800

    calc avg of reviews

--- read.py
+++ read.py
@@ -6,3 +6,9 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
+print('檔案讀取完了，共有',len(data),'筆資料！')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('平均：',sum_len/len(data))
