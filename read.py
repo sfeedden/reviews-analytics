2723339d26e797f107b6eb89c9478cfb92ff6b1d
Steven Liang <gkg.yji@gmail.com>
2020-07-02 00:37:31 +0800

    add word count

--- read.py
+++ read.py
@@ -23,9 +23,35 @@ print(new[0])
 print(new[1])
 
 
+#文字計數
 good = []
 for d in data:
 	if 'good' in d:
 		good.append(d)
 print('一共有：',len(good),'筆留言提及good')
 print(good[0])
+
+good = [d for d in data if 'good' in d] #26~30
+print('總共有--',len(good),'個留言含有good')
+
+wc = {} #word_count
+for d in data:
+	words = d.split() #預設值，為空白鍵
+	for w in words:
+		if w in wc:
+			wc[w] += 1
+		else:
+			wc[w] = 1
+for w in wc:
+	if wc[w] >= 1000000:
+		print(w, wc[w])
+
+while True:
+	w = input('請輸入想查詢的字：')
+	if w == 'q':
+		break
+	if w in wc:
+		print('您所查詢的字是：', w, '；共出現', wc[w], '次 ')
+	else:
+		print('很抱歉，您所查詢的字並未出現')
+print('感謝使用')
